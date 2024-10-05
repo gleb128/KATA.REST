d175c2af2e8b2beea9ff21c5dcaf5e431d05b33c
gleb128 <glebtide@gmail.com>
2024-10-05 23:06:40 +0300

    user панель

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -4,19 +4,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.kata.spring.boot_security.demo.entity.Role;
 import ru.kata.spring.boot_security.demo.entity.User;
+import ru.kata.spring.boot_security.demo.repository.RoleRepository;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 public class UserController {
     @Autowired
     private UserService userService;
+    @Autowired
+    RoleRepository roleRepository;
 
     @GetMapping("/user")
-    public String userInfo(Principal principal, Model model) {
+    public String userInfo(Model model, Principal principal) {
+        model.addAttribute("username", principal.getName());
         User user = (User) userService.loadUserByUsername(principal.getName());
+        List<Role> roles = (List<Role>) roleRepository.findAll();
+        model.addAttribute("roles", roles);
         model.addAttribute("user", user);
         return "User_Info";
     }
